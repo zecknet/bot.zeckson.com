05edb76c60a55e8d795f1689449f0ebf82a7cf23
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2026-03-26 21:31:29 +0700

    fix deploy

--- server.ts
+++ server.ts
@@ -1,47 +1,25 @@
 import './src/config.prod.ts'
-import { handleWebhook, setWebhook } from './server.deno.ts'
-import { bot } from './src/bot.ts'
-import { store } from './src/middleware/store.ts'
-import { config } from './src/config.ts'
+import { handleWebhook } from './server.deno.ts'
 import ServerResponse from './src/server/response.ts'
 
-const DEPLOY_URL = config.BASE_URL
-
 const hello = (req: Request) =>
-	ServerResponse.text(
-		`Hello World!\nRequest url: ${req.url}\n`,
-	)
+    ServerResponse.text(
+        `Hello World!\nRequest url: ${req.url}\n`,
+    )
 
 Deno.serve(async (req) => {
-	const start = Date.now()
-	let response = hello(req)
-	try {
-		const webhookResponse = await handleWebhook(req)
-		response = webhookResponse ?? response
-	} finally {
-		console.debug(
-			`${req.method} ${req.url} - ${response.status} "${response.type}" in ${
-				Date.now() - start
-			}ms`,
-		)
-	}
+    const start = Date.now()
+    let response = hello(req)
+    try {
+        const webhookResponse = await handleWebhook(req)
+        response = webhookResponse ?? response
+    } finally {
+        console.debug(
+            `${req.method} ${req.url} - ${response.status} "${response.type}" in ${
+                Date.now() - start
+            }ms`,
+        )
+    }
 
-	return response
+    return response
 })
-
-console.log(`Deno deploy url: ${DEPLOY_URL}`)
-
-if (DEPLOY_URL) {
-	const key = [`deploy_url`, DEPLOY_URL]
-	const deploymentData = await store.load(key)
-
-	if (deploymentData) {
-		console.log(`Webhook ${DEPLOY_URL} is already set`)
-	} else {
-		const path = `${DEPLOY_URL}/${bot.token}`
-		console.log(`Setting webhook ${path}`)
-		await setWebhook(path)
-		await store.set(key, true)
-	}
-}
-
---
+++ webhook.ts
@@ -0,0 +1,23 @@
+import './src/config.prod.ts'
+import { setWebhook } from './server.deno.ts'
+import { config } from './src/config.ts'
+import { store } from './src/middleware/store.ts'
+
+const DEPLOY_URL = config.BASE_URL
+console.log(`Deno deploy url: ${DEPLOY_URL}`)
+
+if (!DEPLOY_URL) {
+    throw new Error('DEPLOY_URL is not set')
+}
+
+const key = [`deploy_url`, DEPLOY_URL]
+const deploymentData = await store.load(key)
+
+if (deploymentData.value === DEPLOY_URL) {
+    console.log(`Webhook ${DEPLOY_URL} is already set`)
+} else {
+    // Save
+    await setWebhook(`${DEPLOY_URL}/${bot.token}`)
+    await store.save(key, DEPLOY_URL)
+}
+
