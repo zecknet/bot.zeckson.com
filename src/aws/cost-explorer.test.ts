9d4b518fa35f88d87aad109dce16ef0a3e64c1e3
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2026-06-10 18:08:12 +0300

    fix tests

--- src/aws/cost-explorer.test.ts
+++ src/aws/cost-explorer.test.ts
@@ -2,7 +2,14 @@ import '../config.local.ts'
 
 import { getDailyCosts, getMTDCost } from './cost-explorer.ts'
 
-Deno.test('CostExplorer', async () => {
-	console.log('Daily costs:', await getDailyCosts())
-	console.log('MTD cost:', await getMTDCost())
+Deno.test({
+	name: 'CostExplorer',
+	ignore: true,
+	permissions: {
+		read: true,
+	},
+	async fn() {
+		console.log('Daily costs:', await getDailyCosts())
+		console.log('MTD cost:', await getMTDCost())
+	},
 })
